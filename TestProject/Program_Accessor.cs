7cf44213cf5883a777658b36b8dca1f230f95c50
Vladimir Khudnitsky <xputnikx@gmail.com>
2012-06-21 00:30:31 +0300

    small update old commit

---
+++ TestProject/Program_Accessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestProject
+{
+    class Program_Accessor
+    {
+        internal void Render()
+        {
+           /// throw new NotImplementedException();
+        }
+
+        internal static void Main(string[] args)
+        {
+            ///throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/Program_Accessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests
+{
+    class Program_Accessor
+    {
+        internal static void Main(string[] args)
+        {
+        }
+
+        internal void Render()
+        {
+        }
+    }
+}
